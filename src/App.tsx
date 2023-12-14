587cd122da801db5074c2b4f4e227d92aa76ecc9
Tomás Arrativel <arrativeltomas@gmail.com>
2023-12-14 10:44:54 -0300

    trying to get the values for card

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,27 @@ import './App.css';
 import { CardsContainer, FormContainer } from './containers/exports';
 
 function App() {
+  let cardNum = '',
+    name = '',
+    expM = '',
+    expY = '',
+    cvc = '';
   return (
     <main>
-      <CardsContainer />
-      <FormContainer />
+      <CardsContainer
+        cardNum={cardNum}
+        name={name}
+        expM={expM}
+        expY={expY}
+        cvc={cvc}
+      />
+      <FormContainer
+        cardNum={cardNum}
+        name={name}
+        expM={expM}
+        expY={expY}
+        cvc={cvc}
+      />
     </main>
   );
 }
