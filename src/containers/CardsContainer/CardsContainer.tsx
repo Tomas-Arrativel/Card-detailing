d89036236a56ee95560f6947ace871867b461677
Tomás Arrativel <arrativeltomas@gmail.com>
2023-12-14 10:44:37 -0300

    making the cards

--- src/containers/CardsContainer/CardsContainer.tsx
+++ src/containers/CardsContainer/CardsContainer.tsx
@@ -1,10 +1,37 @@
 import { Card } from '../../components/exports';
 import styles from './CardsContainer.module.css';
 
-const CardsContainer = () => {
+interface CardsProps {
+  cardNum: string;
+  name: string;
+  expM: string;
+  expY: string;
+  cvc: string;
+}
+
+const CardsContainer = ({ cardNum, name, expM, expY, cvc }: CardsProps) => {
   return (
     <section className={styles.cardsCont}>
-      <Card />
+      <div className={styles.background}></div>
+
+      <div className={styles.cards}>
+        <div className={styles.frontCard}>
+          <img src='./images/card-logo.svg' alt='' />
+          <p className={styles.cardNum}>
+            {cardNum == '' ? '0000 0000 0000 0000' : cardNum}
+          </p>
+          <p className={styles.name}>{name == '' ? 'Tomás Arrativel' : name}</p>
+          <div className={styles.expDate}>
+            <p>
+              <span>{expM == '' ? '00' : expM}</span> /{' '}
+              <span>{expY == '' ? '00' : expY}</span>
+            </p>
+          </div>
+        </div>
+        <div className={styles.backCard}>
+          <p className={styles.cvc}>{cvc == '' ? '000' : cvc}</p>
+        </div>
+      </div>
     </section>
   );
 };
