5e73297a5e6382fa1ab1e0e305506e776718732d
Tomás Arrativel <arrativeltomas@gmail.com>
2023-12-19 10:51:22 -0300

    Fully responsive site

--- src/containers/CardsContainer/CardsContainer.tsx
+++ src/containers/CardsContainer/CardsContainer.tsx
@@ -13,30 +13,30 @@ interface CardsProps {
 const CardsContainer = ({ creditCard }: CardsProps) => {
   return (
     <section className={styles.cardsCont}>
-      <div className={styles.background}></div>
-
-      <div className={styles.cards}>
-        <div className={styles.frontCard}>
-          <img src='./images/card-logo.svg' alt='' />
-          <p className={styles.cardNum}>
-            {creditCard.cardNumber == ''
-              ? '0000 0000 0000 0000'
-              : creditCard.cardNumber}
-          </p>
-          <p className={styles.name}>
-            {creditCard.name == '' ? 'Jane Appleseed' : creditCard.name}
-          </p>
-          <div className={styles.expDate}>
-            <p>
-              <span>{creditCard.expM == '' ? '00' : creditCard.expM}</span> /{' '}
-              <span>{creditCard.expY == '' ? '00' : creditCard.expY}</span>
+      <div className={styles.background}>
+        <div className={styles.cards}>
+          <div className={styles.frontCard}>
+            <img src='./images/card-logo.svg' alt='' />
+            <p className={styles.cardNum}>
+              {creditCard.cardNumber == ''
+                ? '0000 0000 0000 0000'
+                : creditCard.cardNumber}
+            </p>
+            <p className={styles.name}>
+              {creditCard.name == '' ? 'Jane Appleseed' : creditCard.name}
+            </p>
+            <div className={styles.expDate}>
+              <p>
+                <span>{creditCard.expM == '' ? '00' : creditCard.expM}</span> /{' '}
+                <span>{creditCard.expY == '' ? '00' : creditCard.expY}</span>
+              </p>
+            </div>
+          </div>
+          <div className={styles.backCard}>
+            <p className={styles.cvc}>
+              {creditCard.cvc == '' ? '000' : creditCard.cvc}
             </p>
           </div>
-        </div>
-        <div className={styles.backCard}>
-          <p className={styles.cvc}>
-            {creditCard.cvc == '' ? '000' : creditCard.cvc}
-          </p>
         </div>
       </div>
     </section>
